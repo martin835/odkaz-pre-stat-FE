324bbdf734ed1ec991351e0f373aeed85600b7f8
martin835 <64438132+martin835@users.noreply.github.com>
2022-05-09 14:52:07 +0200

    warning bar added

--- odkaz-prestat-fe/src/components/header/Header.jsx
+++ odkaz-prestat-fe/src/components/header/Header.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import HeaderCaption from "./caption/HeaderCaption";
+import HeaderCaption from "./HeaderCaption";
+import WarningBar from "./WarningBar";
 
 function Header() {
   return (
-    <header className="idsk-header-web " data-module="idsk-header-web">
-      <div className="idsk-header-web__tricolor"></div>
+    <>
+      <header className="idsk-header-web " data-module="idsk-header-web">
+        <WarningBar />
+        <div className="idsk-header-web__tricolor"></div>
+        <div className="idsk-header-web__brand ">
+          <div className="govuk-width-container">
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-full">
+                <HeaderCaption />
 
-      <div className="idsk-header-web__brand ">
-        <div className="govuk-width-container">
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full">
-              <HeaderCaption />
-
-              {/* 
+                {/* 
               <div className="idsk-header-web__brand-language">
                 <button
                   className="idsk-header-web__brand-language-button"
@@ -63,38 +65,106 @@ function Header() {
                   </li>
                 </ul>
               </div> */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="idsk-header-web__main">
-        <div className="govuk-width-container">
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full govuk-grid-column-one-third-from-desktop">
-              <div className="idsk-header-web__main-headline">
-                <a href="/" title="Odkaz na úvodnú stránku">
-                  <h2 className="govuk-heading-m">Odkaz pre štát</h2>
-                </a>
+        <div className="idsk-header-web__main">
+          <div className="govuk-width-container">
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-full govuk-grid-column-one-third-from-desktop">
+                <div className="idsk-header-web__main-headline">
+                  <a href="/" title="Odkaz na úvodnú stránku">
+                    <h2 className="govuk-heading-m">Odkaz pre štát</h2>
+                  </a>
 
-                <button
-                  className="idsk-button idsk-header-web__main-headline-menu-button idsk-header-web__main-headline-menu-button-service"
-                  aria-label="Rozbaliť menu"
-                  aria-expanded="false"
-                  data-text-for-show="Rozbaliť menu"
-                  data-text-for-hide="Skryť menu"
-                >
-                  <img
-                    src="/public/assets/images/header-web//profile.svg"
-                    alt="Electronic service menu icon"
-                  />
-                  <div className="idsk-header-web__menu-close"></div>
-                </button>
+                  <button
+                    className="idsk-button idsk-header-web__main-headline-menu-button idsk-header-web__main-headline-menu-button-service"
+                    aria-label="Rozbaliť menu"
+                    aria-expanded="false"
+                    data-text-for-show="Rozbaliť menu"
+                    data-text-for-hide="Skryť menu"
+                  >
+                    <img
+                      src="/public/assets/images/header-web//profile.svg"
+                      alt="Electronic service menu icon"
+                    />
+                    <div className="idsk-header-web__menu-close"></div>
+                  </button>
+                </div>
+              </div>
+
+              <div className="govuk-grid-column-two-thirds">
+                <div className="idsk-header-web__main-action">
+                  <div className="idsk-header-web__main--buttons">
+                    <div className="idsk-header-web__main--login ">
+                      <button
+                        type="button"
+                        className="idsk-button idsk-header-web__main--login-loginbtn"
+                        data-module="idsk-button"
+                      >
+                        Prihlásiť sa
+                      </button>
+                      <div className="idsk-header-web__main--login-action">
+                        <img
+                          className="idsk-header-web__main--login-action-profile-img"
+                          src="/public/assets/images/header-web/profile.svg"
+                          alt="Profile image"
+                        />
+                        <div className="idsk-header-web__main--login-action-text">
+                          <span className="govuk-body-s idsk-header-web__main--login-action-text-user-name">
+                            Ing. Jožko Veľký M.A
+                          </span>
+                          <div className="govuk-!-margin-bottom-1">
+                            <a
+                              className="govuk-link idsk-header-web__main--login-action-text-logout idsk-header-web__main--login-logoutbtn"
+                              href="#"
+                              title="odhlásiť"
+                            >
+                              Odhlásiť
+                            </a>
+                            <span> | </span>
+                            <a
+                              className="govuk-link idsk-header-web__main--login-action-text-profile idsk-header-web__main--login-profilebtn"
+                              href="#"
+                              title="profil"
+                            >
+                              Profil
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <button
+                        type="button"
+                        className="idsk-button idsk-header-web__main--login-profilebtn"
+                        data-module="idsk-button"
+                      >
+                        Profil
+                      </button>
+                      <button
+                        type="button"
+                        className="idsk-button idsk-header-web__main--login-logoutbtn"
+                        data-module="idsk-button"
+                      >
+                        Odhlásiť sa
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
 
-            <div className="govuk-grid-column-two-thirds">
-              <div className="idsk-header-web__main-action">
+        <div className="idsk-header-web__nav idsk-header-web__nav--mobile ">
+          <div className="govuk-width-container">
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-full"></div>
+            </div>
+
+            <div className="govuk-grid-row">
+              <div className="govuk-grid-column-full">
                 <div className="idsk-header-web__main--buttons">
                   <div className="idsk-header-web__main--login ">
                     <button
@@ -153,75 +223,8 @@ function Header() {
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="idsk-header-web__nav idsk-header-web__nav--mobile ">
-        <div className="govuk-width-container">
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full"></div>
-          </div>
-
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-full">
-              <div className="idsk-header-web__main--buttons">
-                <div className="idsk-header-web__main--login ">
-                  <button
-                    type="button"
-                    className="idsk-button idsk-header-web__main--login-loginbtn"
-                    data-module="idsk-button"
-                  >
-                    Prihlásiť sa
-                  </button>
-                  <div className="idsk-header-web__main--login-action">
-                    <img
-                      className="idsk-header-web__main--login-action-profile-img"
-                      src="/public/assets/images/header-web/profile.svg"
-                      alt="Profile image"
-                    />
-                    <div className="idsk-header-web__main--login-action-text">
-                      <span className="govuk-body-s idsk-header-web__main--login-action-text-user-name">
-                        Ing. Jožko Veľký M.A
-                      </span>
-                      <div className="govuk-!-margin-bottom-1">
-                        <a
-                          className="govuk-link idsk-header-web__main--login-action-text-logout idsk-header-web__main--login-logoutbtn"
-                          href="#"
-                          title="odhlásiť"
-                        >
-                          Odhlásiť
-                        </a>
-                        <span> | </span>
-                        <a
-                          className="govuk-link idsk-header-web__main--login-action-text-profile idsk-header-web__main--login-profilebtn"
-                          href="#"
-                          title="profil"
-                        >
-                          Profil
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    type="button"
-                    className="idsk-button idsk-header-web__main--login-profilebtn"
-                    data-module="idsk-button"
-                  >
-                    Profil
-                  </button>
-                  <button
-                    type="button"
-                    className="idsk-button idsk-header-web__main--login-logoutbtn"
-                    data-module="idsk-button"
-                  >
-                    Odhlásiť sa
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
 
---
+++ odkaz-prestat-fe/src/components/header/WarningBar.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function WarningBar() {
+  const [showWarningBar, setShowWarningBar] = useState(true);
+
+  return (
+    showWarningBar && (
+      <div class="idsk-header-web__banner ">
+        <div class="govuk-width-container">
+          <div class="govuk-grid-row">
+            <div class="govuk-grid-column-full">
+              <div class="govuk-body-s idsk-header-web__banner-title">
+                Aplikácia vo vývoji.
+              </div>
+              <button
+                class="idsk-header-web__banner-close"
+                aria-label="Zavrieť banner"
+                onClick={() => setShowWarningBar(false)}
+              >
+                <span class="idsk-header-web__menu-close"></span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
+
+export default WarningBar;
