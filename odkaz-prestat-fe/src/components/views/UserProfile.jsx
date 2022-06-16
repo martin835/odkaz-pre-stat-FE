ccfd6e5003b8246826d1197c1f03a0fe56ef2006
martin835 <64438132+martin835@users.noreply.github.com>
2022-06-16 17:39:32 +0200

    uploading image with formData not working

--- odkaz-prestat-fe/src/components/views/UserProfile.jsx
+++ odkaz-prestat-fe/src/components/views/UserProfile.jsx
@@ -1,10 +1,49 @@
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import Login from "../login/Login";
+import { useState } from "react";
 
 function UserProfile(props) {
   const loggedUser = useSelector((state) => state.loggedUser);
   const { t } = useTranslation();
+  const [showUploadImage, setShowUploadImage] = useState(false);
+
+  const formData = new FormData();
+  const getImg = (e) => {
+    formData.append("avatar", e.target.files[0]);
+    console.log("IMAGE: ", formData);
+  };
+
+  const uploadAvatar = async () => {
+    console.log(formData);
+    try {
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_URL}/users/me/avatar`,
+        {
+          method: "POST",
+
+          //credentials: "include",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+          data: formData,
+        }
+      );
+      if (response.ok) {
+        //console.log(response);
+      } else {
+        console.log("login failed");
+        if (response.status === 400) {
+          console.log("bad request");
+        }
+        if (response.status === 404) {
+          console.log("page not found");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return loggedUser ? (
     <div className="govuk-width-container">
@@ -20,10 +59,12 @@ function UserProfile(props) {
               <dt className="govuk-summary-list__key">{t("UserProfile-2")}</dt>
               <dd className="govuk-summary-list__value">{loggedUser.name}</dd>
               <dd className="govuk-summary-list__actions">
-                <a className="govuk-link" href="#">
+                <button
+                  className="govuk-button govuk-button--secondary my-0"
+                  data-module="govuk-button"
+                >
                   {t("UserProfile-3")}
-                  <span className="govuk-visually-hidden"> name</span>
-                </a>
+                </button>
               </dd>
             </div>
             {/* ROW END */}
@@ -35,10 +76,12 @@ function UserProfile(props) {
                 {loggedUser.surname}
               </dd>
               <dd className="govuk-summary-list__actions">
-                <a className="govuk-link" href="#">
+                <button
+                  className="govuk-button govuk-button--secondary my-0"
+                  data-module="govuk-button"
+                >
                   {t("UserProfile-3")}
-                  <span className="govuk-visually-hidden"> name</span>
-                </a>
+                </button>
               </dd>
             </div>
             {/* ROW END */}
@@ -54,17 +97,54 @@ function UserProfile(props) {
               <dt className="govuk-summary-list__key">Avatar</dt>
               <dd className="govuk-summary-list__value">
                 <img
-                  src={loggedUser.avatar}
+                  src={loggedUser?.avatar}
                   className="header-profile-pic"
                   alt="avatar"
                   referrerPolicy="no-referrer"
                 ></img>
               </dd>
               <dd className="govuk-summary-list__actions">
-                <a className="govuk-link" href="#">
+                <button
+                  className="govuk-button govuk-button--secondary my-0"
+                  data-module="govuk-button"
+                  onClick={() => setShowUploadImage(!showUploadImage)}
+                >
                   {t("UserProfile-3")}
-                  <span className="govuk-visually-hidden"> name</span>
-                </a>
+                </button>
+              </dd>
+            </div>
+            {/* ROW END */}
+            {/* ROW START */}
+            <div
+              className={
+                showUploadImage
+                  ? `govuk-summary-list__row`
+                  : `govuk-summary-list__row d-none`
+              }
+            >
+              <dt className="govuk-summary-list__key">Upload new image</dt>
+              <dd className="govuk-summary-list__value">
+                <div className="govuk-form-group">
+                  <label className="govuk-label" htmlFor="file-upload-avatar">
+                    Upload a file
+                  </label>
+                  <input
+                    className="govuk-file-upload"
+                    id="file-upload-avatar"
+                    name="file-upload-avatar"
+                    type="file"
+                    onChange={getImg}
+                  />
+                </div>
+              </dd>
+              <dd className="govuk-summary-list__actions">
+                <button
+                  className="govuk-button govuk-button--secondary my-0"
+                  data-module="govuk-button"
+                  onClick={() => uploadAvatar()}
+                >
+                  Upload
+                </button>
               </dd>
             </div>
             {/* ROW END */}
